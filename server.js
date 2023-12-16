d2c0c0e21b56df6f3088998bf8925bbdf4d4989b
Tin Phan <phanthanhtincr7@gmail.com>
2023-12-16 11:41:13 +0700

    first commit

---
+++ server.js
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.get("/", (req, res) => {
+	res.send("Hello World!");
+});
+
+app.listen(port || 3000, () => {
+	console.log("Server is listening on port ", port);
+});
