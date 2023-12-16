cc8ecd6b5229dd283c9494a36cdc0bbb999689b2
Tin Phan <phanthanhtincr7@gmail.com>
2023-12-16 13:50:50 +0700

    up

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 app.get("/", (req, res) => {
-	res.send("Hello World!");
+	res.send("Hello World1!");
 });
 
 app.listen(port || 3000, () => {
